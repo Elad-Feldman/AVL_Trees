bf18d65631e36f1e691c8ac0fde6e63dfc705360
shellysela <shellysela@gmail.com>
2020-12-09 13:33:24 +0200

    Update splitMeasurement.java

--- src/Mesermeunts/splitMeasurement.java
+++ src/Mesermeunts/splitMeasurement.java
@@ -8,10 +8,10 @@ import com.sun.tools.javac.util.ArrayUtils;
 
 public class splitMeasurement {
 
-    public static long[] randomSplit() {
+    public static long[] randomSplit(int i) {
         // load tree
         AVLTree t = new AVLTree();
-        int[] nodes = sort.createRandArray(1000);
+        int[] nodes = sort.createRandArray(1000*i);
         splitMes.loadTree(nodes, t);
 
         // split
@@ -25,10 +25,10 @@ public class splitMeasurement {
         return new long[]{avgJoin, maxJoin};
     }
 
-    public static long[] leftLeafSplit() {
+    public static long[] leftLeafSplit(int i) {
         // load tree
         AVLTree t = new AVLTree();
-        int[] nodes = sort.createRandArray(1000);
+        int[] nodes = sort.createRandArray(1000*i);
         splitMes.loadTree(nodes, t);
 
         // split
@@ -54,17 +54,27 @@ public class splitMeasurement {
 
 
     public static void main(String[] args) {
-        long[] random = randomSplit();
-        long[] leftLeaf = leftLeafSplit();
-
-        //long[] results = {random[0], random[1], leftLeaf[0], leftLeaf[1]};
-
-        String SPACE ="        ";
-        String SPACE2 ="                            ";
+        long[] random,leftLeaf;
 
 
-        System.out.println("avgJoinRandomSplit" + SPACE + "maxJoinRandomSplit" + SPACE + "avgJoinLeftLeafSplit" + SPACE + "maxJoinLeftLeafSplit" );
-        System.out.println(random[0] + SPACE2 + random[1] + SPACE2 + leftLeaf[0] + SPACE2 + leftLeaf[1] );
+        for (int i = 1; i <= 10; i++) {
+            random = randomSplit(i);
+            leftLeaf = leftLeafSplit(i);
+
+            //long[] results = {random[0], random[1], leftLeaf[0], leftLeaf[1]};
+
+            String SPACE ="        ";
+            String SPACE2 ="                            ";
+
+
+            System.out.println("n = "+10000*i);
+            System.out.println("avgJoinRandomSplit" + SPACE + "maxJoinRandomSplit" + SPACE + "avgJoinLeftLeafSplit" + SPACE + "maxJoinLeftLeafSplit" );
+            System.out.println(random[0] + SPACE2 + random[1] + SPACE2 + leftLeaf[0] + SPACE2 + leftLeaf[1] );
+            System.out.println();
+
+        }
+
+
 
 
     }
